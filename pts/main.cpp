129825d15751170bbf9df5fdac012f04f782c951
Mikyas Tesfamichael <mickyastesfamichael@gmail.com>
2020-05-01 14:38:44 +0300

    tested libxlsxwriter with actual PTS data; talk inserting function still not implemented

--- pts/main.cpp
+++ pts/main.cpp
@@ -10,6 +10,8 @@
 #include <vector>
 
 using namespace pts;
+using sqlite_orm::where;
+using sqlite_orm::c;
 
 int main(int argc, char *argv[])
 {
@@ -22,25 +24,23 @@ int main(int argc, char *argv[])
 //    return a.exec();
 //    //return 0;
 
-    const std::string CONG_NAME = "Addis Sefer";
-
     lxw_workbook* workbook = workbook_new("PTS.xlsx");
-    lxw_worksheet* worksheet = workbook_add_worksheet(workbook, CONG_NAME.c_str());
-
-    Elder James("James", "Jasper", "Gerund", "0222", 1, 1, true);
-    Elder Jones("Jones", "Jerud", "Jerk", "0111", 1, 1, true);
-    Elder Jonah("Clark", "Click", "Cunt", "0333", 1, 1, true);
-
-    std::vector<Elder> elders = {James, Jones, Jonah};
-
     pts::ExcelGenerator excelGenerator;
 
-    std::cout << "inserting congregation name..." << std::endl;
-    excelGenerator.insertCongregationName(worksheet, CONG_NAME.c_str());
-    std::cout << "inserting columns and elders names..." << std::endl;
-    excelGenerator.insertColumnsAndElderNames(worksheet, elders);
-    std::cout << "inserting instruction messages..." << std::endl;
-    excelGenerator.insertInstructionMessage(worksheet);
+    for (auto congregation : pts::PTSDatabase::getAllCongregations()) {
+        lxw_worksheet* worksheet = workbook_add_worksheet(workbook, congregation.getName().c_str());
+
+        std::vector<Elder> elders = pts::PTSDatabase::getStorage()
+                .get_all<Elder>(where(c(&Elder::getCongregationId) = congregation.getId()));
+
+        std::cout << "inserting congregation name..." << std::endl;
+        excelGenerator.insertCongregationName(worksheet, congregation.getName().c_str());
+        std::cout << "inserting columns and elders names..." << std::endl;
+        excelGenerator.insertColumnsAndElderNames(worksheet, elders);
+        std::cout << "inserting instruction messages..." << std::endl;
+        excelGenerator.insertInstructionMessage(worksheet);
+    }
+
     std::cout << "saving workbook..." << std::endl;
     workbook_close(workbook);
 
